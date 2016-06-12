a417ca00fa24bf33a01aeca69aaff3bd362bc8c2
Daniel Buscombe <dbuscombe@usgs.gov>
2016-06-12 12:14:15 -0700

    Sun Jun 12 12:14:15 MST 2016

--- im_regis_alt.py
+++ im_regis_alt.py
@@ -1,85 +1,105 @@
-from scipy import ndimage
-from scipy.spatial import cKDTree
-from scipy.optimize import fmin
 
-def register(im1,im2):
-    
-    # initialize the transformation matrix
-    T = [[1.0,0.0],[0.0,1.0]]
-    
-    # extract features from first image  
-    edges1 = ndimage.sobel(im1)
-    features1 = dstack(where(edges1.T > edges1.mean()))[0]
-    kd = cKDTree(features1)
-    
-    def errfunction(T,kd=kd,im2=im2):
-        
-        # transform the image
-        img = ndimage.interpolation.affine_transform(im2,T.reshape(2,2))
-        
-        # extract features from transformed image 
-        edges = ndimage.sobel(img)
-        features = dstack(where(edges.T > edges.mean()))[0]
-        
-        dst,idx = kd.query([features])
-        
-        # measure the distances between features.
-        return sum(dst**2)
-        
-    T = fmin(errfunction,T,xtol=1.e-6,ftol=1.e-6,maxfun=5000,maxiter=5000)
-    
-    return (T,ndimage.interpolation.affine_transform(im2,T.reshape(2,2)))
+master = '/run/media/dbuscombe/MASTER/GCMRC/SANDBAR_REMOTECAMERAS/RC0220Ra/deployment7/RC0220Ra_20130922_1353.jpg'
 
-im1 = imread("sample_images/IMG1.png")[:,:,0]
-im2 = imread("sample_images/IMG2.png")[:,:,0]
+im1 = imread(master)[:,:,0]
+im2 = imread("RC0220Ra_surveydata/RC0220Ra_20150924_1141.JPG")[:,:,0]
 
-im1 = ndimage.filters.uniform_filter(im1,5)
-im2 = ndimage.filters.uniform_filter(im2,5)
 
-im1 -= im1.min(); im1 /= im1.max() 
-im2 -= im2.min(); im2 /= im2.max()
 
-(T,rimg) = register(im1,im2)
+#from scipy import ndimage
+#from scipy.spatial import cKDTree
+#from scipy.optimize import fmin
 
-#%%
-figure(1)
-clf()
-s = subplot(1,1,1)
-title('Gradient Image with Detected Features')
-imshow(edges,'bone')
-edges = ndimage.sobel(im1)
-features = dstack(where( (edges.T > 0.45) & (edges.T < 0.55) ))[0]
-plot(features[:,0],features[:,1],'+',color='red')
-xlim([0,im1.shape[0]])
-ylim([0,im1.shape[1]])
-s.axes.get_xaxis().set_visible(False)
-s.axes.get_yaxis().set_visible(False)
-#%%
-figure(2)
-clf()
+#import matplotlib.pyplot as plt
+#import numpy as np
 
-s = subplot(2,2,1)
-title('Original Image')
-imshow(im1,'bone')
-s.axes.get_xaxis().set_visible(False)
-s.axes.get_yaxis().set_visible(False)
+#def register(im1,im2):
+#    
+#    # initialize the transformation matrix
+#    T = np.asarray([[1.0,0.0],[0.0,1.0]])
+#    
+#    # extract features from first image  
+#    edges1 = ndimage.sobel(im1)
+#    features1 = np.dstack(np.where(edges1.T > edges1.mean()))[0]
+#    kd = cKDTree(features1)
+#    
+#    def errfunction(T,kd=kd,im2=im2):
+#        
+#        # transform the image
+#        img = ndimage.interpolation.affine_transform(im2,T.reshape(2,2))
+#        
+#        # extract features from transformed image 
+#        edges = ndimage.sobel(img)
+#        features = np.dstack(np.where(edges.T > edges.mean()))[0]
+#        
+#        dst,idx = kd.query([features])
+#        
+#        # measure the distances between features.
+#        return sum(dst**2)
+#        
+#    T = fmin(errfunction,T,xtol=1.e-6,ftol=1.e-6,maxfun=5000,maxiter=5000)
+#    
+#    return (T,ndimage.interpolation.affine_transform(im2,T.reshape(2,2)))
 
-s = subplot(2,2,2)
-title('Matching Image')
-imshow(im2,'bone')
-s.axes.get_xaxis().set_visible(False)
-s.axes.get_yaxis().set_visible(False)
+#master = '/run/media/dbuscombe/MASTER/GCMRC/SANDBAR_REMOTECAMERAS/RC0220Ra/deployment7/RC0220Ra_20130922_1353.jpg'
 
-s = subplot(2,2,3)
-title('Matching Image subtracted from Original Image')
-imshow(im2-im1,'jet')
-s.axes.get_xaxis().set_visible(False)
-s.axes.get_yaxis().set_visible(False)
+#im1 = imread(master)[:,:,0]
+#im2 = imread("RC0220Ra_surveydata/RC0220Ra_20150924_1141.JPG")[:,:,0]
 
-s = subplot(2,2,4)
-title('Registred Image subtracted from Original Image')
-imshow(rimg-im1,'jet')
-s.axes.get_xaxis().set_visible(False)
-s.axes.get_yaxis().set_visible(False)
+#im1 = ndimage.filters.uniform_filter(im1,5)
+#im2 = ndimage.filters.uniform_filter(im2,5)
 
-show()
+#im1 = im1 - im1.min()
+#im1 = im1 / im1.max()
+
+#im2 = im2 - im2.min()
+#im2 = im2 / im2.max()
+
+
+##im1 -= im1.min(); im1 /= im1.max() 
+##im2 -= im2.min(); im2 /= im2.max()
+
+#(T,rimg) = register(im1,im2)
+
+##%%
+#figure(1)
+#clf()
+#s = subplot(1,1,1)
+#title('Gradient Image with Detected Features')
+#imshow(edges,'bone')
+#edges = ndimage.sobel(im1)
+#features = dstack(where( (edges.T > 0.45) & (edges.T < 0.55) ))[0]
+#plot(features[:,0],features[:,1],'+',color='red')
+#xlim([0,im1.shape[0]])
+#ylim([0,im1.shape[1]])
+#s.axes.get_xaxis().set_visible(False)
+#s.axes.get_yaxis().set_visible(False)
+##%%
+#figure(2)
+#clf()
+
+#s = subplot(2,2,1)
+#title('Original Image')
+#imshow(im1,'bone')
+#s.axes.get_xaxis().set_visible(False)
+#s.axes.get_yaxis().set_visible(False)
+
+#s = subplot(2,2,2)
+#title('Matching Image')
+#imshow(im2,'bone')
+#s.axes.get_xaxis().set_visible(False)
+#s.axes.get_yaxis().set_visible(False)
+
+#s = subplot(2,2,3)
+#title('Matching Image subtracted from Original Image')
+#imshow(im2-im1,'jet')
+#s.axes.get_xaxis().set_visible(False)
+#s.axes.get_yaxis().set_visible(False)
+
+#s = subplot(2,2,4)
+#title('Registred Image subtracted from Original Image')
+#imshow(rimg-im1,'jet')
+#s.axes.get_xaxis().set_visible(False)
+#s.axes.get_yaxis().set_visible(False)
+
+#show()
